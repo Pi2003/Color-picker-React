ee2cbdf25dd0a43eb4ade6f89197f49a1fb1ddb3
Pinak <pdtendulkar140203@gmail.com>
2023-06-18 15:47:42 +0530

    Commit_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import "./App.css";
+export default function App() {
+  const [color, setColor] = useState("lightblue");
+  const [hidden, setHidden] = useState(false);
+  const pickerStyle = {
+    default: {
+      picker: {
+        position: "absolute",
+        bottom: "30px",
+        left: "100px"
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: color }}>
+      <div className="container">
+        {hidden && (
+          <SketchPicker
+            styles={pickerStyle}
+            color={color}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
+          />
+        )}
+
+        <button onClick={() => setHidden(!hidden)}>
+          {hidden ? "Close Color Picker" : "Open Color Picker"}
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
